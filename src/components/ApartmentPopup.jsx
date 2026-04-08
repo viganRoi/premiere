8886cb756a982bc3c3ecd675181c4fabbf6ef820
Diarduriqi <diar.roitiv@gmail.com>
2026-04-08 14:14:37 +0200

    popup name

--- src/components/ApartmentPopup.jsx
+++ src/components/ApartmentPopup.jsx
@@ -37,7 +37,7 @@ const ApartmentPopup = ({ apartment, mousePosition }) => {
               color: "#f5cba7",
             }}
           >
-            {apartment.title}
+            {apartment.name}
           </Typography>
         </Box>
 
